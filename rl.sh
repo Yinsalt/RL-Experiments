76cee8c268ddedb4d7eb92f980ebfabacef6a9d1
Yinsalt <v.o.y.94@pm.me>
2026-01-20 18:58:14 +0100

    Revise rl.sh for conda package installations
    Updated installation instructions for PyTorch and dependencies.

--- rl.sh
+++ rl.sh
@@ -1,18 +1,19 @@
-# 1. Conda env (oder venv, was du magst)
+# 1. Env erstellen
 conda create -n drl python=3.11 -y
 conda activate drl
 
-# 2. PyTorch mit CUDA für deine 3060
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+# 2. PyTorch über conda (mit CUDA)
+conda install pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
 
-# 3. Repo klonen
+# 3. Was conda hat, über conda
+conda install numpy opencv tensorboard pytest -c conda-forge
+
+# 4. Repo klonen
 git clone https://github.com/PacktPublishing/Deep-Reinforcement-Learning-Hands-On-Third-Edition.git
 cd Deep-Reinforcement-Learning-Hands-On-Third-Edition
 
-# 4. Dependencies
-pip install -r requirements.txt
-
-# 5. Test ob GPU erkannt wird
-python -c "import torch; print(torch.cuda.is_available(), torch.cuda.get_device_name(0))"
+# 5. Rest über pip (nur einmal, am Ende)
+pip install gymnasium[atari,classic-control,accept-rom-license] \
+    pytorch-ignite ptan stable-baselines3 torchrl "ray[tune]" moviepy mypy
 #pip install autorom
 #autorom --accept-license
